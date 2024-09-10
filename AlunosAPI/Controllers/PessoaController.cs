bb8601609e700bfd687eec2bdf082724ab037c6f
diogolopes758 <diogo.lopesdecarvalholopes@gmail.com>
2024-09-10 19:56:11 -0300

    criação da tabela Pessoa

---
+++ AlunosAPI/Controllers/PessoaController.cs
@@ -0,0 +1,60 @@
+﻿using AlunosAPI.Models;
+using AlunosAPI.Repositories;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlunosAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        private readonly PessoaRepository _pessoaRepository;
+
+        public PessoaController(PessoaRepository pessoaRepository)
+        {
+            _pessoaRepository = pessoaRepository;
+        }
+
+        // GET: api/<PessoaController>
+        [HttpGet]
+        public async Task<IEnumerable<Pessoa>> Listar()
+        {
+            return await _pessoaRepository.ListarTodasPessoas();
+        }
+
+        // GET api/<PessoaController>/5
+        [HttpGet("{id}")]
+        public async Task<Pessoa> BuscarPorId(int id)
+        {
+            return await _pessoaRepository.BuscarPorId(id);
+        }
+
+        // POST api/<PessoaController>
+        [HttpPost]
+        public async void Post([FromBody] Pessoa value)
+        {
+            await _pessoaRepository.Salvar(value);
+
+        }
+
+        // PUT api/<PessoaController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Pessoa dados)
+        {
+            dados.Id= id; ;
+            await _pessoaRepository.atualizar(dados);
+            return Ok();
+
+        }
+
+        // DELETE api/<PessoaController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _pessoaRepository.DeletarPorId(id);
+            return Ok();
+        }
+    }
+
+}
